dfd4f9f3d8a85c05b8263dc04fcb4520d01e2d84
Aiman Farhan <aiman.faruk@surialabs.com>
2022-05-23 09:51:25 +0800

    added base html and css

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-A-Sketch-1
+PseoduCode
---
+++ script.js
@@ -0,0 +1,34 @@
+// Get all required elements from document
+const colorPicker = document.querySelector("#box-bg-color")
+const rainbowBtn = document.querySelector(".rainbowBtn")
+const eraserBtn = document.querySelector(".eraserBtn")
+const clearBoxesBtn = document.querySelector(".clearBoxesBtn")
+const boxSizeInput = document.querySelector("#box-size")
+const boxesContainer = document.querySelector(".boxesContainer")
+
+
+//DOM Manipulation functions
+
+function setGridColumnRow () {
+
+}
+
+function setBoxColor () {
+
+}
+
+function setRainbowMode () {
+
+}
+
+function setEraserMode () {
+
+}
+
+function resetBoxes () {
+
+}
+
+function appendBoxesToBoxesContainer () {
+  
+}
